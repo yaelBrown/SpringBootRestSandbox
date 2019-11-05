34444efd62da28454b10bfc06bea5d9646a55a07
yaelBrown <yaelrbrown@gmail.com>
2019-11-05 15:36:12 -0600

    Created and tested other crud operations.

--- src/main/java/com/yael/Dao/PersonDao.java
+++ src/main/java/com/yael/Dao/PersonDao.java
@@ -23,8 +23,73 @@ public class PersonDao {
         };
     }
 
+    private static int findIdByName(String name) {
+        int idx = 0;
+
+        for(Person p : people.values()) {
+            if (p.getName().equalsIgnoreCase(name)) {
+                idx = p.getId();
+            }
+        }
+
+        return idx;
+    }
+
     public Collection<Person> getAllPeople() {
         return this.people.values();
+    }
+
+    public Collection<Person> getPersonById(int id) {
+        return (Collection<Person>) this.people.get(id);
+    }
+
+    public static void addPerson(String name) {
+        int idx = people.size() + 1;
+        people.put(idx, new Person(idx, name));
+    }
+
+    public static void deletePerson(String name) {
+        int idx = findIdByName(name);
+        people.remove(idx);
+    }
+
+    public static void updatePerson(String preName, String postName) {
+        int idx = findIdByName(preName);
+        people.put(idx, new Person(idx, postName));
+    }
+
+    public static void main(String[] args) {
+        System.out.println("===Add Person===");
+        addPerson("Katy");
+        addPerson("Pumpkin");
+        addPerson("Elmo");
+
+//        System.out.println(people.get(2).getId());
+
+        for(Person p : people.values()) {
+            System.out.println(p.getId() + " " + p.getName());
+        }
+
+        System.out.println("===Delete Person===");
+
+        deletePerson("Yael");
+
+        for(Person p : people.values()) {
+            System.out.println(p.getId() + " " + p.getName());
+        }
+
+        System.out.println("===Update Person===");
+
+        updatePerson("Katy", "Katherine");
+
+        for(Person p : people.values()) {
+            System.out.println(p.getId() + " " + p.getName());
+        }
+
+        System.out.println("===Find person by ID===");
+
+
+
     }
 
 }
